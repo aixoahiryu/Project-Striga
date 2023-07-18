91b2bd14ecfab22268d003eec1d4daf8fc343eac
U-ADMIN\Administrator <aixoahiryu@gmail.com>
2023-07-18 16:35:49 +0700

    updated file workspace

--- [ Core ]/[ Program ]/UI/Bar/Left/File.py
+++ [ Core ]/[ Program ]/UI/Bar/Left/File.py
@@ -19,6 +19,7 @@ dialog = ''
 
 addicon = False
 darkmode = True
+tooltip = False
 colorbg = "#000000" if darkmode else "#ffffff"
 colorfg = "#ffffff" if darkmode else "#000000"
 
@@ -43,19 +44,87 @@ root.grid_rowconfigure(1, weight=1)
 #root.configure(highlightbackground="#000000")
 #root.configure(highlightcolor="white")
 
+overflow = Toplevel(sidebar)
+overflow.title('Icon overflow')
+overflow.geometry("-0+25")
+overflow.overrideredirect(1)
+overflow.configure(bg=colorbg)
+#overflow.transient(taskbar)
+overflow.attributes('-topmost', True)
+imggemini=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\geminiw.png')
+Button(overflow, text=' Thaumiel', relief='flat', background=colorbg, foreground='#c9c9c9', image=imggemini, compound='left').grid(column=0, row=0, sticky='NW')
+imgeye=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\eyew.png')
+Button(overflow, text=' The eye', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgeye, compound='left').grid(column=0, row=1, sticky='NW')
+imgmoon=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\moonw.png')
+Button(overflow, text=' Moon cycle', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgmoon, compound='left').grid(column=0, row=2, sticky='NW')
+imgsun=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\sunw.png')
+Button(overflow, text=' Sun cycle', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgsun, compound='left').grid(column=0, row=3, sticky='NW')
+imgdice=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\dicew.png')
+Button(overflow, text=' Chaos theory', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgdice, compound='left').grid(column=0, row=4, sticky='NW')
+imgcalendar=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\calendarw.png')
+Button(overflow, text=' Calendar', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgcalendar, compound='left').grid(column=0, row=5, sticky='NW')
+imghorse=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\horsew.png')
+Button(overflow, text=' Strategy', relief='flat', background=colorbg, foreground='#c9c9c9', image=imghorse, compound='left').grid(column=0, row=6, sticky='NW')
+imgwave=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\wave2w.png')
+Button(overflow, text=' Flunctuation', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgwave, compound='left').grid(column=0, row=7, sticky='NW')
+overflow.withdraw()
+overflow_on = False
+
+def toggle_overflow():
+	global overflow_on
+	if overflow_on: overflow.withdraw()
+	else: overflow.deiconify()
+	overflow_on = not overflow_on
 taskbar = Toplevel(sidebar)
 taskbar.attributes('-topmost', True)
 taskbar.title('Taskbar')
 taskbar.geometry("1366x25+1+0")
 taskbar.overrideredirect(1)
+#taskbar.configure(bg=colorbg, bd=1, relief='groove')
 taskbar.configure(bg=colorbg)
 Label(taskbar, text=r'[ Workspace ]', background=colorbg, foreground=colorfg).place(relx=0.48, rely=0)
+appframe = Frame(taskbar, bg=colorbg)
+appframe.grid(sticky='NSW', column=0, row=0)
+#Button(appframe, text='1', relief='flat', background='#3c3c3c', foreground=colorfg).grid(column=0, row=0, sticky='NW')
+Button(appframe, text='1', relief='flat', background='#253B34', foreground='#6effbe').grid(column=0, row=0, sticky='NW')
+Button(appframe, text='2', relief='flat', background=colorbg, foreground=colorfg).grid(column=1, row=0, sticky='NW')
+Button(appframe, text='3', relief='flat', background=colorbg, foreground=colorfg).grid(column=2, row=0, sticky='NW')
+Button(appframe, text='4', relief='flat', background=colorbg, foreground=colorfg).grid(column=3, row=0, sticky='NW')
+Button(appframe, text='#', relief='flat', background=colorbg, foreground=colorfg).grid(column=4, row=0, sticky='NW')
+imgcode=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\neon\code.gif')
+Button(appframe, text=' Code', relief='flat', background=colorbg, foreground=colorfg, image=imgcode, compound='left').grid(column=5, row=0, sticky='NW')
+imgterm=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\neon\term3.gif')
+Button(appframe, text=' Hacking', relief='flat', background=colorbg, foreground=colorfg, image=imgterm, compound='left').grid(column=6, row=0, sticky='NW')
+imgfile=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\neon\file.gif')
+Button(appframe, text=' File', relief='flat', background=colorbg, foreground='#6effbe', image=imgfile, compound='left').grid(column=7, row=0, sticky='NW')
+imglink=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\neon\qr.gif')
+Button(appframe, text=' Network', relief='flat', background=colorbg, foreground=colorfg, image=imglink, compound='left').grid(column=8, row=0, sticky='NW')
+trayframe = Frame(taskbar, bg=colorbg)
+trayframe.grid(sticky='NSE', column=1, row=0)
+taskbar.grid_columnconfigure(1, weight=1)
+imgcpu=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\cpuw.png')
+Button(trayframe, text=' 13%', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgcpu, compound='left').grid(column=0, row=0, sticky='NW')
+imghdd=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\hddw.png')
+Button(trayframe, text=' 322G', relief='flat', background=colorbg, foreground='#c9c9c9', image=imghdd, compound='left').grid(column=1, row=0, sticky='NW')
+imgnetwork=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\networkw.png')
+Button(trayframe, text=' 0.7 MB/s', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgnetwork, compound='left').grid(column=2, row=0, sticky='NW')
+imgram=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\ramw.png')
+Button(trayframe, text=' 2.2 GB', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgram, compound='left').grid(column=3, row=0, sticky='NW')
+imgtemp=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\tempw.png')
+Button(trayframe, text=' 33°C', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgtemp, compound='left').grid(column=4, row=0, sticky='NW')
+imgvolume=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\volumew.png')
+Button(trayframe, text=' Ballad', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgvolume, compound='left').grid(column=5, row=0, sticky='NW')
+imgmenu=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\menuw.png')
+btnoverflow = Button(trayframe, text='', relief='flat', background=colorbg, foreground='#c9c9c9', image=imgmenu, compound='none')
+btnoverflow.grid(column=6, row=0, sticky='NW')
+btnoverflow.bind("<Button-1>", lambda event: toggle_overflow())
 #taskbar.bind("<Button-1>", lambda event: (root.withdraw(), sidebar.geometry("1366x1+0+0"), taskbar.geometry("1366x24+0+1")))
 taskbar.bind("<Button-1>", lambda event: root.withdraw())
 
+
 titlepanel = Toplevel(sidebar)
 titlepanel.attributes('-topmost', True)
-titlepanel.attributes('-alpha', 0.7)
+titlepanel.attributes('-alpha', 0.77)
 titlepanel.title('ASCII')
 titlepanel.geometry("1366x740+1+25")
 titlepanel.overrideredirect(1)
@@ -69,29 +138,6 @@ msg1=r''' __________________________________________________________
 |                                                        | |
 |                                                        |_|
 |________________________________________________________|/|
-
-ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-ZZZZZZZZZZZZds+:--------:+sdNZZZZZZZZZZZ
-ZZZZZZZZZs:-+sdNZZZZZZZZNdy+--oZZZZZZZZZ
-ZZZZZZh:` /ZZZZZZZZZZZZZZZZZZ+ `-yZZZZZZ
-ZZZZd--hN``--sNZZZZZZZZZZNy:..`Zd:.hZZZZ
-ZZZ+`yZZZy hd+./hZZZZZZh/.+dd sZZZh`/ZZZ
-ZZ:.ZZZZZZ:.NZZh/.+dd+./hZZZ--ZZZZZZ--NZ
-Z+`ZZZZZZZN`+ZZZZZ-  .dZZZZo ZZZZZZZN.:Z
-d yZZZZZZZZy dNy:.oZNs--sNZ oZZZZZZZZh h
-/`ZZZZZZZZZZ.`.+dZZZZZZZ+.``NZZZZZZZZZ-:
-.:ZZZZZZZd+./`oZZZZZZZZZZs /.+dZZZZZZZ/`
-.:ZZZZZo.:yNZs dZZZZZZZZZ`oZNy:.oZZZZZ/`
-/`ZNy:.oZZZZZZ--ZZZZZZZZ:.ZZZZZNs--sNZ.:
-d -` :++++++++: /++++++/ :++++++++:  : h
-Z+ yddddddddddd+ yddddy /dddddddddddy`/Z
-ZZ/.ZZZZZZZZZZZZ.-ZZZZ/.NZZZZZZZZZZZ.:NZ
-ZZZo`sZZZZZZZZZZd sZZy hZZZZZZZZZZy`+ZZZ
-ZZZZd--hZZZZZZZZZ+`ZN`/ZZZZZZZZZh--hZZZZ
-ZZZZZZh:.oZZZZZZZN.:/`NZZZZZZZs.:hZZZZZZ
-ZZZZZZZZNs:./shZZZh  yZZNds/.:sZZZZZZZZZ
-ZZZZZZZZZZZZdy+/---``---:+sdZZZZZZZZZZZZ
-ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 '''
 msg2=r'''
 |     .-.
@@ -143,7 +189,10 @@ Day and Night, Night and Day. Endless, finite, two are one.
 ⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⠁⡀⡀⡀⡀⠰⠇⡀⣟⡀⡀⡿⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
 I have the key to open the door. I can die to set me free.
 
-┌─[ Monolith:13 ]─[ /dev/void ]
+┌─[ Monolith:13 ]─[ /dev/void ]                        ───────┐
+|                                                             |
+|                                                             |
+|                                                             |
 | 
 | 
 | 
@@ -160,16 +209,14 @@ I have the key to open the door. I can die to set me free.
 | 
 | 
 | 
-| 
-| 
-| 
-| 
-| 
-| 
-└── ‡ breach --cluster #6effbe
+|                                                             |
+|                                                             |
+|                                                             |
+└── ‡ breach --cluster #6effbe                         ───────┘
 '''
-txt1 = Message(titlepanel, text=msg1, width=444, bg=colorbg, fg=colorfg, font=("Lucida Console", 8, "normal"))
+txt1 = Frame(titlepanel, bg=colorbg)
 txt1.grid(row=0, column=0, sticky='NW')
+Message(txt1, text=msg1, width=444, bg=colorbg, fg=colorfg, font=("Lucida Console", 8, "normal")).grid(row=0, column=0, sticky='NW')
 txt2 = Message(titlepanel, text=msg2, width=444, bg=colorbg, fg=colorfg, font=("Lucida Console", 8, "normal"))
 txt2.grid(row=0, column=1, sticky='NEW')
 txt3 = Message(titlepanel, text=msg3, width=444, bg=colorbg, fg=colorfg, font=("Lucida Console", 8, "normal"))
@@ -177,25 +224,55 @@ txt3.grid(row=0, column=2, sticky='NE')
 titlepanel.grid_columnconfigure(0, weight=1)
 titlepanel.grid_columnconfigure(1, weight=1)
 titlepanel.grid_columnconfigure(2, weight=1)
+txt1frame = Frame(txt1, bg=colorbg)
+txt1frame.grid(row=1, column=0, sticky='NW')
+img1=PhotoImage(file=ZLCORE+r'\Toolbar\_\[ Program ]\[ Source ]\gif\bw\geminiw.png')
+Button(txt1frame, text=' Thaumiel', relief='flat', background=colorbg, foreground='#c9c9c9', image=img1, compound='left').grid(column=0, row=0, sticky='NW')
+
+popup = Toplevel(sidebar)
+popup.title('Popup')
+popup.geometry("+10+350")
+popup.overrideredirect(1)
+popup.attributes('-alpha', 0.77)
+popup.configure(bg=colorbg)
+popup.attributes('-topmost', True)
+popupmsg='''
+File workspace
+'''
+Message(popup, text=popupmsg, bg=colorbg, fg=colorfg, font=("Lucida Console", 8, "normal")).grid(row=0, column=0, sticky='NW')
+popup.withdraw()
 
 #root.tk.call("source", r"C:\Users\Administrator\Desktop\tcl\theme\Forest\void.tcl")
 style = ttk.Style()
-#style.theme_use('void')
+style.theme_use('alt')
 style.configure("Treeview", background=colorbg, foreground=colorfg, fieldbackground=colorbg)
-style.map('Treeview', background=[ ('selected', '#6effbe') ], foreground=[ ('selected', '#000000') ])
+style.map('Treeview', background=[('selected', '#6effbe')], foreground=[('selected', '#000000')])
+style.configure("TCombobox", background=colorbg, foreground=colorfg, fieldbackground=colorbg, highlightcolor=colorfg, arrowcolor=colorfg)
+style.map('TCombobox', background=[('readonly', colorbg)], foreground=[('readonly', colorfg)], fieldbackground=[('readonly', colorbg)], highlightcolor=[('readonly', colorfg)], arrowcolor=[('readonly', colorfg)] )
+style.configure("TScrollbar", background=colorbg, foreground=colorfg, fieldbackground=colorbg, highlightcolor=colorfg, troughcolor=colorbg, arrowcolor=colorfg)
+style.configure("Menu", background=colorbg, foreground=colorfg, fieldbackground=colorbg, highlightcolor=colorfg)
+
 
 def preview_clipboard():
 	try:
 		global msg2_2
 		#msg2_2 = msg2_2+root.clipboard_get()
-		msg2_2 = msg2+r'┌─[ Clipboard:Text ]─[ /dev/void ]'+'\n'
+		msg2_2 = msg2+r'┌─[ Clipboard:xclip ]─[ /dev/clipboard ]'+'\n'
 		temp1 = root.clipboard_get().split('\n')
 		for i in temp1[:10]:
 			msg2_2 = msg2_2+r'| '+i[:55]+'\n'
+		if len(temp1)<10:
+			for i in range(0, (10-len(temp1))): msg2_2 = msg2_2+r'| '+'\n'
 		msg2_2 = msg2_2+r'└── ‡ limit --head 10'
 		txt2.configure(text=msg2_2)
 		preview_file()
-	except: txt2.configure(text=msg2+'Clipboard failure')
+	except: #txt2.configure(text=msg2+'Clipboard failure')
+		msg2_2 = msg2+r'┌─[ Clipboard:xclip ]─[ /dev/clipboard ]'+'\n'
+		msg2_2 = msg2_2+r'| '+'Cliboard failure \n'
+		for i in range(1, 10): msg2_2 = msg2_2+r'| '+'\n'
+		msg2_2 = msg2_2+r'└── ‡ limit --head 10'
+		txt2.configure(text=msg2_2)
+		preview_file()
 
 def preview_file():
 	try:
@@ -205,15 +282,16 @@ def preview_file():
 			filecontent = file.read()
 			file.close()
 			filecontent = filecontent.split("\n")
-			for i in filecontent[:33]:
+			for i in filecontent[:30]:
 				msg2_3 = msg2_3+r'| '+i[:55]+'\n'
-			msg2_3 = msg2_3+r'└── ‡ limit --head 33'
+			if len(filecontent)<30:
+				for i in range(0, (30-len(filecontent))): msg2_3 = msg2_3+r'| '+'\n'
+			msg2_3 = msg2_3+r'└── ‡ limit --head 30'
 			txt2.configure(text=msg2_3)
 	except: txt2.configure(text=msg2_2+'Preview failure')
 
 def toggle_sidebar(*event):
-	global hidden
-	global root
+	global hidden, root, overflow_on
 	
 	if (hidden == False):
 		root.withdraw()
@@ -229,6 +307,9 @@ def toggle_sidebar(*event):
 		sidebar.geometry("1x740+0+0")
 		preview_clipboard()
 	hidden = not hidden
+	if overflow_on:
+		overflow.withdraw()
+		overflow_on = False
 
 	root.focus_set()
 
@@ -360,6 +441,7 @@ def open_popup():
 	dialog.geometry("250x100+333+25")
 	dialog.resizable(False, False)
 	dialog.title("Child Window")
+	dialog.transient(root)
 	dialog.columnconfigure(0, weight=1)
 	Label(dialog, text='Enter File or Folder name').grid()
 	Entry(dialog, textvariable=newFileName).grid(column=0, pady=10, sticky='NSEW')
@@ -373,13 +455,15 @@ def newFileOrFolder():
 	else:
 		os.mkdir(os.path.join(fullpath2, newFileName.get()))
 	dialog.destroy()
-	populate_tree(tree, tree.focus())
+	if os.path.isdir(fullpath): populate_tree(tree, tree.focus())
+	else: populate_tree(tree, tree.parent(tree.focus()))
 
 newFileName = StringVar(root, "", 'new_name')
 #currentPath = StringVar(root, name='currentPath', value=pathlib.Path.cwd())
 #currentPath.trace('w', pathChange)
 
-def workspace_click():
+def workspace_select():
+	global fullpath
 	combo1.configure(state="readonly")
 	file = open(ZLCORE+r'\Toolbar\F\[ Workspace ]\[ Sidebar ]\Internal.txt', mode='r')
 	filecontent = file.read()
@@ -387,14 +471,13 @@ def workspace_click():
 	filecontent = filecontent.split("\n")
 	combo1['values'] = filecontent
 
-def workspace_select():
-	global fullpath
 	combo1.configure(width=len(combo1.get())+1)
 	fullpath = ZLCORE+'\\Toolbar\\F\\[ Workspace ]\\[ Sidebar ]\\'+combo1.get()
 	if os.path.isdir(fullpath):
 		os.chdir(fullpath)
 		tree.delete(tree.get_children(''))
 		populate_roots(tree)
+
 
 sidebar.bind("<Button-1>", toggle_sidebar)
 root.bind("<Alt-Up>", goBack)
@@ -431,7 +514,7 @@ frame3.grid_columnconfigure(1, weight=1)
 combo1 = ttk.Combobox(frame3_2, state="readonly", values=['--------------'], width=1)
 combo1.grid(sticky='E', row=0, column=0)
 combo1.bind('<Button-3>', lambda e: combo1.configure(state="normal"))
-combo1.bind('<Button-1>', lambda e: workspace_click())
+combo1.bind('<Button-1>', lambda e: workspace_select())
 combo1.bind('<<ComboboxSelected>>', lambda e: workspace_select())
 
 vsb = ttk.Scrollbar(frame2, orient="vertical")
@@ -458,6 +541,7 @@ tree.bind('<Double-Button-1>', change_dir)
 tree.bind('<ButtonRelease-1>', selectItem)
 
 
+#menubar = Menu(root, tearoff=0, background='#ffffff', foreground='#000000', activebackground='#000000', activeforeground='#ffffff')
 menubar = Menu(root, tearoff=0)
 menubar.add_command(label="New", command=open_popup)
 menubar.add_separator()
@@ -471,4 +555,7 @@ menubar.add_command(label="Copy path", command=lambda: (root.clipboard_clear(),r
 #root.config(menu=menubar)
 tree.bind("<Button-3>", lambda event: menubar.post(event.x_root, event.y_root))
 
+if tooltip:
+	sidebar.bind("<Enter>", lambda e: popup.deiconify() if hidden else print('hidden'))
+	sidebar.bind("<Leave>", lambda e: popup.withdraw() if hidden else print('hidden'))
 sidebar.mainloop()
